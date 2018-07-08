2846e56dd03e98ff72ced25a429536447a618506
mac <1350587807@qq.com>
2018-07-08 10:48:52 +0800

    Initial Commit

---
+++ SwiftMusicDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMusicDemo
+//
+//  Created by mac on 2018/7/8.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
